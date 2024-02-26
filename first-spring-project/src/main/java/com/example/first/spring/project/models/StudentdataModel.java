d57f622c6c4d4385b2890ce01ca40fa2f1e5f9d2
Animesh <animeshsharma42002@gmail.com>
2024-02-26 20:50:36 +0530

    student routers created

---
+++ first-spring-project/src/main/java/com/example/first/spring/project/models/StudentdataModel.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public  class StudentDatsModel{
+    public  String Id;
+    public  String name;
+    public  int age;
+    public String Aadhaar;
+    public String University;
+}
+
